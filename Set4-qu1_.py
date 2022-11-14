f5cd842931945d0b64c0b76ee05a56869f311b9c
shainakhatun <116898401+shainakhatun@users.noreply.github.com>
2022-11-14 23:24:42 +0530

    Add files via upload

---
+++ Set4-qu1_.py
@@ -0,0 +1,18 @@
+# calculated total marks as the sum of marks list.
+# But this code doesn't runs. Debug this code and then submit it.
+
+# marks=['10','32','42','65','67','89','76','38','67']
+# i=0
+# s=marks[i]
+# sum=0
+# b=[]
+# while i<len(marks):
+#     sum=sum+int(marks[i])
+#     b.append(sum)
+#     i=i+1
+# print(sum)
+    
+
+
+
+
